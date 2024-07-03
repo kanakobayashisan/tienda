a17a666e25d4ba0e516f30fd412e423a4ae242fd
kanakobayashisan <kanakobayashisan@gmail.com>
2024-07-03 22:21:31 +0200

    agregando nombres

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mi tienda
+          jimmy vilca ramos
         </p>
         <a
           className="App-link"
